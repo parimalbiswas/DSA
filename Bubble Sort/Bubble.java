800360aea300199fc9dc5e6a927e2415ea42d103
PARIMAL BISWAS <biswasparimal01@gmail.com>
2023-01-22 21:44:08 +0530

    .

--- Bubble Sort/Bubble.java
+++ Bubble Sort/Bubble.java
@@ -1,9 +1,32 @@
-/**
- * Bubble
- */
 public class Bubble {
 
     public static void main(String[] args) {
-        
+        int arr[] = { 7, 8, 3, 1, 2 };
+
+        // Bubble Sort
+        for (int i = 0; i < arr.length - 1; i++) 
+        {
+            for (int j = 0; j < arr.length - 1 - i; j++) 
+            {
+                if (arr[j] > arr[j + 1]) 
+                {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArray(arr);
     }
+
+    private static void printArray(int[] arr) 
+    {
+
+        for (int i = 0; i < arr.length; i++) 
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
 }
